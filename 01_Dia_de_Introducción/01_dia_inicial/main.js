ebe1790f16442a5c4c67283c8cb9fe9ac9d09999
fnkbsk <igoico10@gmail.com>
2024-01-28 17:51:55 -0300

    Introduccion

---
+++ 01_Dia_de_Introducción/01_dia_inicial/main.js
@@ -0,0 +1,4 @@
+// Se pueden acceder a valores de variables de otro archivo .js
+console.log(firstName, lastName, country, city, age, isMarried)
+console.log(gravity, boilingPoint, PI)
+console.log(name, job, live)
---
+++ 01_Dia_de_Introducción/01_dia_inicial/variable.js
@@ -0,0 +1,18 @@
+// Declarando diferentes variables de diferente tipos de datos.
+
+let firstName = 'Iñaki' // Nombre de una persona
+let lastName = 'Goicoechea' // Apellellido de una persona
+let country = 'Argentina' // País
+let city = 'Villa Constitución' // Ciudad
+let age = 21 // Edad en años
+let isMarried = true
+
+// Declarando variables con valores numericos
+const gravity = 9.81 // Gravedad de la tierra en m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Las variables tambien se pueden declarar en una linea separados con una coma
+let name = 'Gabriela',
+    job = 'Teacher',
+    live = 'Finland'
